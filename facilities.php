9a8498493b757a6a9511cd2cbd2205c667d7e98d
Devika18-04 <123755403+Devika18-04@users.noreply.github.com>
2024-01-04 20:08:12 +0530

    Add files via upload

---
+++ facilities.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HotelBooking-facilities</title>
+    <?php require('inc/links.php'); ?>
+    <style>
+      .pop:hover{
+        border-top-color: var(--teal) !important;
+        transform:scale(1.03);
+        transition:all 0.3s;
+      }
+    </style>
+    
+</head>
+<body class="bg-light">
+
+<?php require('inc/header.php'); ?>
+
+<div class="my-5 px-4">
+  <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+  <div class="h-line bg-dark"></div>
+</div>
+<p class="text-center mt-3">
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+   Expedita hic sed dolore autem quia quae<br> quidem doloribus sunt sit facilis?
+</p>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shawdow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+        <img src="images/features/wifi.svg" width="40px">
+        <h5 class="m-0 ms-3">wifi</h5>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, 
+          placeat sapiente voluptatum natus aut ipsum error harum magnam consequuntur nemo?</p>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shawdow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+        <img src="images/features/wifi.svg" width="40px">
+        <h5 class="m-0 ms-3">wifi</h5>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, 
+          placeat sapiente voluptatum natus aut ipsum error harum magnam consequuntur nemo?</p>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shawdow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+        <img src="images/features/wifi.svg" width="40px">
+        <h5 class="m-0 ms-3">wifi</h5>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, 
+          placeat sapiente voluptatum natus aut ipsum error harum magnam consequuntur nemo?</p>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shawdow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+        <img src="images/features/wifi.svg" width="40px">
+        <h5 class="m-0 ms-3">wifi</h5>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, 
+          placeat sapiente voluptatum natus aut ipsum error harum magnam consequuntur nemo?</p>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shawdow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+        <img src="images/features/wifi.svg" width="40px">
+        <h5 class="m-0 ms-3">wifi</h5>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, 
+          placeat sapiente voluptatum natus aut ipsum error harum magnam consequuntur nemo?</p>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 mb-5 px-4">
+      <div class="bg-white rounded shawdow p-4 border-top border-4 border-dark pop">
+        <div class="d-flex align-items-center mb-2">
+        <img src="images/features/wifi.svg" width="40px">
+        <h5 class="m-0 ms-3">wifi</h5>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, 
+          placeat sapiente voluptatum natus aut ipsum error harum magnam consequuntur nemo?</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require('inc/footer.php'); ?>
+
+</body>
+</html>
